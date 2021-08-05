853eb60b302eb9e91624345d67efceefd3904128
TalkitiveDiv <abhipieznomnomnom@gmail.com>
2021-08-05 18:44:47 -0400

    removed vite

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,3 @@
-import '/src/styles/styles.css'
-const app = document.getElementById('app');
-const appContent =
-`
-<div class="form">
-<input type="number" class="form__input" id = "txtMinNum" placeholder="Enter a small number...">
-<input type="number" class="form__input" id = "txtMaxNum" placeholder="Enter a big number...">
-<label id = "lblRandInt">Random Number is: 0</label>
-<button class="btn btnGen" id = "btnGen"><p>Genarate!</p></button>
-<div class="repoContainer"><button type="button" id ="btnRepo" class="button">
-<span class="button__text">Source</span>
-<span class="button__icon">
-<ion-icon name="code-slash-outline"></ion-icon>
-</span>
-</button></div>
-<div>Icons made by <a href="https://www.flaticon.com/authors/kiranshastry" title="Kiranshastry">Kiranshastry</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-</div>
-`
-app.innerHTML = appContent
 const txtMinNum = document.getElementById('txtMinNum')
 const txtMaxNum = document.getElementById('txtMaxNum')
 const btnGen = document.getElementById('btnGen');
@@ -29,10 +10,11 @@ let getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-btnGen.addEventListener('click', () =>{
-lblRandInt.innerText = "Random Number is: " + getRandomInt(txtMinNum.value, txtMaxNum.value)
-})
-
+let showRandomInt = (min, max) =>{
+  lblRandInt.innerText = "Random Number is: " + getRandomInt(min, max)
+} 
+btnGen.addEventListener('click', showRandomInt(txtMinNum.value,txtMaxNum.value))
+document.addEventListener('DOMContentLoaded', showRandomInt(1,100))
 btnRepo.addEventListener('click', (e) =>{
   if(e.ctrlKey){window.open(repo)}
   else{window.location.href = repo}
